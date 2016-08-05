1f8e831f37b95352ddf6c743a69ce4144fca9384
Richard Rodger <richard@ricebridge.com>
2016-08-05 18:54:16 +0100

    first working

--- gate-executor.js
+++ gate-executor.js
@@ -1,215 +1,147 @@
-/* Copyright (c) 2014-2015 Richard Rodger, MIT License */
-/* jshint node:true, asi:true, eqnull:true */
+/* Copyright (c) 2014-2016 Richard Rodger, MIT License */
 "use strict";
 
 
-var events = require('events')
-var util   = require('util')
-
-var _      = require('lodash')
-var async  = require('async')
-var error  = require('eraro')({package:'gate-executor'})
+function make_GateExecutor (options) {
+  var ge = new GateExecutor()
+  return ge
+}
 
 
-util.inherits( GateExecutor, events.EventEmitter )
+var gec = 0
 
-// Create new GateExecutor
-// options:
-//    * _timeout_:   take timeout
-//    * _trace_:     true => built in tracing, function => custom tracing
-//    * _error_:     function for unexpected errors, default: emit: 'error'
-//    * _msg_codes_: custom tracing code names
-function GateExecutor( options ) {
+function GateExecutor () {
   var self = this
-  events.EventEmitter.call(self)
+  self.id = ++gec
 
-  options = _.extend({
-    timeout: 33333,
-    trace:   false,
-    stubs:   {Date:{}},
+  var q = []
 
-    error: function(err) {
-      self.emit('error',err)
-    },
-
-    clear: function() {
-      self.emit('clear')
-    },
-  },options)
-
-  options.msg_codes = _.extend({
-    msg_codes: {
-      timeout:   'task-timeout',
-      error:     'task-error',
-      callback:  'task-callback',
-      execute:   'task-execute',
-      abandoned: 'task-abandoned'
-    }
-  },options.msg_codes)
-
-
-  var set_timeout   = options.stubs.setTimeout   || setTimeout
-  var clear_timeout = options.stubs.clearTimeout || clearTimeout
-  var now           = options.stubs.Date.now     || Date.now
-
-  var q = async.queue(work,1)
-
-  var gated    = false
-  var waiters  = []
-  var inflight = 0
-
-  var runtrace = !!options.trace
-  self.tracelog = runtrace ? (_.isFunction(options.trace) ? null : []) : null
-
-  var tr = !runtrace ? _.noop :
-        (_.isFunction(options.trace) ? options.trace : function() {
-          var args = Array.prototype.slice.call(arguments)
-          args.unshift(now())
-          self.tracelog.push( args )
-        })
-
-
-  q.drain = function(){
-    /* jshint boss:true */
-
-    tr('ungate',gated,inflight)
-    gated = false
-
-    var task = null
-    while( task = waiters.shift() ) {
-      work(task,task.cb)
-    }
+  var p = {
+    m: {},
+    s: []
   }
 
+  var s = {
+    pc: 10000*gec,
+    idc: 0,
+    gate: false,
+    running: false,
+    clear: null,
+    firstclear: null
+  }
 
-  function work( task, done ) {
-    tr('work',gated,inflight,task.id,task.desc)
+  function process (whence) {
+    s.pc += 1
 
-    function check_clear() {
-      inflight--
+    function next() {
+      var res = false
+      var work
 
-      //console.log(inflight, waiters.length, q.length())
-      //if( 0 == inflight && 0 === waiters.length && 0 === q.length() ) {
-      if( self.clear() ) {
-        tr('clear',gated,inflight)
-        options.clear()
+      if (!s.gate) { 
+        // console.log(s.pc,'Q',whence,self.state())
+        work = q.shift()
       }
+
+      if (work) {
+        p.m[work.id] = work
+        p.s.push(work)
+        s.gate = work.gate
+        // console.log(s.pc,'W',whence,self.state())
+
+        work.fn(function () {
+          p.m[work.id].done = true
+          delete p.m[work.id]
+          while (p.s[0] && p.s[0].done) {
+            p.s.shift()
+          }
+          // console.log(s.pc,'D',whence,self.state())
+
+          if (work.gate) {
+            s.gate = false
+          }
+
+          if (0 === q.length && 0 === p.s.length) {
+            if (s.firstclear) {
+              var fc = s.firstclear
+              s.firstclear = null
+              fc()
+            }
+            if (s.clear) {
+              s.clear()
+            }
+          }
+
+          setImmediate(function () {
+            process('fn-done:'+work.id)
+          })
+        })
+
+        res = true
+      }
+      return res
     }
 
-    setImmediate( function(){
-      var completed = false
-      var timedout  = false
-      var timeout = (typeof task.timeout === 'number') ? task.timeout : options.timeout
+    while (next()) {}
+  }
 
-      if( done ) {
-        var toref = set_timeout(function(){
-          timedout = true
-          if( completed ) return;
-
-          tr('timeout',gated,inflight,task.id,task.desc)
-          task.time.end = now()
-
-          var err = new Error(
-            '[TIMEOUT:'+task.id+':'+
-              timeout+'<'+task.time.end+'-'+task.time.start+':'+
-              task.desc+']')
-
-          err.timeout = true
-
-          err = error(err,options.msg_codes.timeout,task)
-
-          try {
-            done(err,null)
-          }
-          catch(e) {
-            options.error(error(e,options.msg_codes.callback,task))
-          }
-
-          check_clear()
-        }, timeout)
-      }
-
-      task.time = {start:now()}
-
-      try {
-        var task_start = Date.now()
-        task.fn(function(err,out){
-          var args = Array.prototype.slice.call(arguments)
-
-          completed = true
-          if( timedout ) return
-
-          tr('done',gated,inflight,task.id,task.desc,Date.now()-task_start)
-          task.time.end = now()
-
-          if( toref ) {
-            clear_timeout(toref)
-          }
-
-          if( err ) {
-            args[0] = error(err,options.msg_codes.error,task)
-            args[1] = args[1] || null
-          }
-
-          if( done ) {
-            try {
-              done.apply(null,args)
-            }
-            catch(e) {
-              options.error(error(e,options.msg_codes.callback,task))
-            }
-          }
-
-          check_clear()
-        })
-      }
-      catch(e) {
-        if( toref ) {
-          clear_timeout(toref)
-        }
-
-        var et = error(e,options.msg_codes.execute,task)
-        try {
-          done(et,null)
-        }
-        catch(e) {
-          options.error(et)
-          options.error(error(e,options.msg_codes.abandoned,task))
-        }
-
-        check_clear()
-      }
+  self.start = function (firstclear) {
+    setImmediate(function () {
+      s.running = true
+      s.firstclear = firstclear
+      process('start')
     })
   }
 
+  self.clear = function (done) {
+    s.clear = done
+  }
 
-  self.execute = function( task ) {
-    inflight++
-
-    if( task.gate ) {
-      tr('gate',gated,inflight,task.id,task.desc)
-      gated = true
-      q.push(task, task.cb)
-    }
-    else if( gated && !task.ungate ) {
-      tr('wait',gated,inflight,task.id,task.desc)
-      waiters.push( task )
-    }
-    else {
-      tr('run',gated,inflight,task.id,task.desc)
-      work( task, task.cb )
+  self.add = function (work) {
+    s.idc += 1
+    work.id = s.idc
+    work.ge = self.id
+    q.push(work)
+    // console.log(s.pc,'A',self.state())
+    
+    if (s.running) {
+      setImmediate(function () {
+        process('add:'+work.id)
+      })
     }
   }
 
+  self.gate = function () {
+    var ge = new GateExecutor()
+    var fn = function gate (done) {
+      ge.start(done)
+    }
 
-  self.clear = function() {
-    return ( 0 == inflight && 0 === waiters.length && 0 === q.length() )
+    self.add({gate:ge, fn:fn})
+
+    return ge
   }
 
-  return self
+  self.state = function () {
+    var o = []
+    for (var i = 0; i < p.s.length; ++i) {
+      var qe = p.s[i]
+      if (!qe.done) {
+        o.push({s:'a', ge: qe.ge, fnn:qe.fn.name, wid:qe.id})
+      }
+    }
+    for (var i = 0; i < q.length; ++i) {
+      var qe = q[i]
+      if (qe.gate) {
+        o.push(qe.gate.state())
+      }
+      else {
+        o.push({s:'w', ge: qe.ge, fnn:qe.fn.name, wid:qe.id})
+      }
+    }
+    return o
+  }
 }
 
 
-module.exports = function( options) {
-  return new GateExecutor(options)
-}
+
+module.exports = make_GateExecutor
--- test/gate-executor.test.js
+++ test/gate-executor.test.js
@@ -1,7 +1,7 @@
-/* Copyright (c) 2014-2015 Richard Rodger, MIT License */
+/* Copyright (c) 2014-2016 Richard Rodger, MIT License */
 "use strict";
 
-var util   = require('util')
+var Util   = require('util')
 
 var Lab = require('lab')
 var Code = require('code')
@@ -11,450 +11,80 @@ var describe = lab.describe
 var it = lab.it
 var expect = Code.expect
 
-var _ = require('lodash')
-
-var executor = require('..')
-
-// timerstub broken on node >0.10
-//var timerstub = require('timerstub')
-
-var timerstub = {
-  setTimeout:  setTimeout,
-  setInterval: setInterval,
-  Date:        Date,
-  wait: function(dur,fn){
-    setTimeout(fn,dur)
-  }
-}
+var GateExecutor = require('..')
 
 
-describe('executor', function(){
-
+describe('gate-executor', function(){
   it('happy', function(done) {
-    if( ~process.version.indexOf('0.11.') ) return done();
+    var log = []
 
-    var e0 = executor({
-      trace:true,
-      timeout:150,
-      stubs:timerstub
+    var ge = GateExecutor()
+
+    ge.add({fn: function aa (d) {log.push('aa'); d()}})
+    ge.add({fn: function bb (d) {log.push('bb'); d()}})
+
+
+    var ge2 = ge.gate()
+    ge2.add({fn: function ccc (d) {log.push('ccc'); d()}})
+
+    ge.add({fn: function dd (d) {log.push('dd'); d()}})
+
+    ge2.add({fn: function ddd (d) {log.push('ddd'); d()}})
+
+    expect(ge.state()).to.deep.equal([ 
+      { s: 'w', ge: 1, fnn: 'aa', wid: 1 },
+      { s: 'w', ge: 1, fnn: 'bb', wid: 2 },
+      [ { s: 'w', ge: 2, fnn: 'ccc', wid: 1 },
+        { s: 'w', ge: 2, fnn: 'ddd', wid: 2 } ],
+      { s: 'w', ge: 1, fnn: 'dd', wid: 4 } ])
+
+    ge2.clear(function () {
+      expect(log).to.deep.equal(['aa', 'bb', 'ccc', 'ddd'])      
+      expect(ge.state()).to.deep.equal([
+        { s: 'w', ge: 1, fnn: 'dd', wid: 4 } ])
     })
 
-    var printlog = []
-
-    function print(err,out){
-      if( err ) return printlog.push('ERROR: '+err)
-      printlog.push(''+out)
-    }
-
-    function mfn(a,d) {
-      var f = function(cb){
-        timerstub.setTimeout(function(){
-          if( 'b'==a ) return cb(new Error('B'));
-          cb(null,a)
-        },d)
-      }
-      return f
-    }
-
-    var start = timerstub.Date.now()
-    e0.execute({id:'a',fn:mfn('a', 25),  cb:print})
-    e0.execute({id:'b',fn:mfn('b', 25),  cb:print})
-    e0.execute({id:'c',fn:mfn('cG',50), cb:print,gate:true})
-    e0.execute({id:'d',fn:mfn('d', 25),  cb:print})
-
-    timerstub.setTimeout( function(){
-      e0.execute({id:'e',fn:mfn('eG', 50), cb:print,gate:true})
-      e0.execute({id:'f',fn:mfn('fG', 50), cb:print,gate:true})
-      e0.execute({id:'g',fn:mfn('g',  25),  cb:print})
-      e0.execute({id:'h',fn:mfn('h',  25),  cb:print})
-
-      e0.execute({id:'i',fn:mfn('i',175),cb:print})
-    },100)
-
-    timerstub.setTimeout( function(){
-      e0.execute({id:'j',fn:mfn('j',25),cb:print})
-      e0.execute({id:'k',fn:mfn('k',25),cb:print})
-    },350)
-
-
-    // hopefully a temporary hack until timerstub works properly with node 0.11
-    function fixfuzz(mod,str) {
-      str = str.replace(/\d+/g, function(m){ return mod*Math.floor(parseInt(m,10)/mod) })
-      //console.log('\nA:'+str)
-      str = str.replace(/\d\d\d/g, function(m){ return (2*mod)*Math.ceil(parseInt(m,10)/(mod*2)) })
-      //console.log('\nB:'+str)
-      str = str.replace(/150/g,'160')
-      str = str.replace(/200/g,'210')
-      str = str.replace(/350/g,'360')
-      return str;
-    }
-
-
-    timerstub.setTimeout(function(){
-      //console.log( util.inspect(printlog).replace(/\s+/g,' ') )
-      printlog[8] = printlog[8].replace(/\[TIMEOUT.*\]/,'[TIMEOUT]')
-
-      expect(printlog).to.deep.equal([ 'a', 'ERROR: Error: gate-executor: B',
-         'cG', 'd', 'eG', 'fG', 'g', 'h',
-         'ERROR: Error: gate-executor: [TIMEOUT]', 'j', 'k' ])
-
-    },400)
-
-    timerstub.wait(450, done)
-  })
-
-
-  it('no-callback', function (done){
-    var e1 = executor({
-      trace:true,
-      timeout:30,
-      stubs:timerstub
-    })
-
-    var t1 = false
-    var start = timerstub.Date.now()
-    e1.execute({id:'a',fn:function(done){t1=true;done()}})
-    timerstub.wait(90,function(){
-      expect(t1).to.be.true()
+    ge.clear(function () {
+      expect(log).to.deep.equal(['aa', 'bb', 'ccc', 'ddd', 'dd'])      
       done()
     })
+
+    ge.start()
   })
 
+/*
+  it('traverse', function(done) {
 
-  it('ignore-gate', function (done){
-    var e1 = executor({
-      trace:true,
-      timeout:20,
-      stubs:timerstub
-    })
+    var p = [
+      {type:'add'}
+    ]
 
-    var seq = ''
+    console.log(build(p))
 
-    e1.execute({id:'a', fn:function(done){ seq+='a';done() }})
-
-    e1.execute({id:'b', gate: true, fn: function(done){
-      seq += 'b'
-
-      e1.execute({id:'c', ungate:true, fn: function(done2){
-        seq += 'c'
-
-        done2()
-        done()
-      }})
-
-      e1.execute({id: 'd', gate:true, fn:function(done3){
-        seq+='d'
-        timerstub.setTimeout(done3,20)
-      }})
-
-    }})
-    e1.execute({id:'e',fn:function(done){ seq+='e';done()}})
-
-    timerstub.wait(40,function(){
-      //console.log('SEQ '+seq)
-
-      expect(seq).to.equal('abcde')
-      done()
-    })
-  })
-
-
-  it('timeouts', function (fin) {
-    var e1 = executor({
-      trace:true,
-      timeout:100,
-      stubs:timerstub
-    })
-
-    var log = []
-
-    function mfn (id,t) {
-      return function(done) {
-        log.push(id)
-        if( t ) {
-          setTimeout(function(){done(null,id)},t)
+    function build (path) {
+      var log = []
+      var ge = [GateExecutor()]
+      for (var i = 0; i < path.length; i++) {
+        var part = path[i]
+        
+        if( 'add' === part.type ) {
+          ge[part.ge].add({
+            fn: function (done) {
+              log.push('p:'+i+';g:'+part.ge)
+              done()
+            }
+          })
         }
-        else done(null,id)
-      }
-    }
-
-    function mcb (id) {
-      return function(err,out) {
-        //console.log('c',id,err,out)
-        log.push('c'+id+'~'+(err&&err.timeout)+'~'+out)
-      }
-    }
-
-    var cI = 0
-    e1.on('clear', function () {
-      cI++
-      log.push('c'+cI)
-      //console.log(cI, e1.tracelog)
-      //console.log(log)
-
-      if( 2 === cI ) {
-        //console.log(log)
-
-        expect(log).to.deep.equal(
-          [ 0,
-            1,
-            'c0~null~0',
-            'c1~true~null',
-            'c1',
-            5,
-            'c5~null~5',
-            2,
-            'c2~null~2',
-            3,
-            'c3~true~null',
-            6,
-            'c6~null~6',
-            4,
-            'c4~null~4',
-            7,
-            'c7~null~7',
-            'c2' ]
-        )
-        fin()
-      }
-    })
-
-    // seq: 0,1
-    e1.execute({id:0,fn:mfn(0,50),cb:mcb(0)})
-    e1.execute({id:1,fn:mfn(1,200),cb:mcb(1)})
-
-    setTimeout( function() {
-
-      // seq: 5,2,3,6,4,7
-      e1.execute({id:2,gate:true,fn:mfn(2,10),cb:mcb(2)})
-      e1.execute({id:3,gate:true,fn:mfn(3,200),cb:mcb(3)})
-      e1.execute({id:4,fn:mfn(4),cb:mcb(4)})
-      e1.execute({id:5,ungate:true,fn:mfn(5),cb:mcb(5)})
-      e1.execute({id:6,gate:true,fn:mfn(6,10),cb:mcb(6)})
-      e1.execute({id:7,fn:mfn(7),cb:mcb(7,10)})
-    }, 200)
-  })
-
-  it('can override timeout', function (fin) {
-    var e1 = executor({
-      trace:true,
-      timeout:5000,
-      stubs:timerstub
-    })
-
-    var log = []
-
-    function mfn (id,t) {
-      return function(done) {
-        log.push(id)
-        if( t ) {
-          setTimeout(function(){done(null,id)},t)
+        else if( 'gate' === part.type ) {
+          ge[ge.length] = ge[part.ge].gate()
         }
-        else done(null,id)
+      }
+
+      return {
+        log: log,
+        ge: ge[0]
       }
     }
-
-    function mcb (id) {
-      return function(err,out) {
-        //console.log('c',id,err,out)
-        log.push('c'+id+'~'+(err&&err.timeout)+'~'+out)
-      }
-    }
-
-    var cI = 0
-    e1.on('clear', function () {
-      cI++
-      log.push('c'+cI)
-      //console.log(cI, e1.tracelog)
-      //console.log(log)
-
-      if( 2 === cI ) {
-        //console.log(log)
-
-        expect(log).to.deep.equal(
-          [ 0,
-            1,
-            'c0~null~0',
-            'c1~true~null',
-            'c1',
-            5,
-            'c5~null~5',
-            2,
-            'c2~null~2',
-            3,
-            'c3~true~null',
-            6,
-            'c6~null~6',
-            4,
-            'c4~null~4',
-            7,
-            'c7~null~7',
-            'c2' ]
-        )
-        fin()
-      }
-    })
-
-    // seq: 0,1
-    e1.execute({id:0,fn:mfn(0,50),cb:mcb(0),timeout:100})
-    e1.execute({id:1,fn:mfn(1,200),cb:mcb(1),timeout:100})
-
-    setTimeout( function() {
-
-      // seq: 5,2,3,6,4,7
-      e1.execute({id:2,gate:true,fn:mfn(2,10),cb:mcb(2),timeout:100})
-      e1.execute({id:3,gate:true,fn:mfn(3,200),cb:mcb(3),timeout:100})
-      e1.execute({id:4,fn:mfn(4),cb:mcb(4),timeout:100})
-      e1.execute({id:5,ungate:true,fn:mfn(5),cb:mcb(5),timeout:100})
-      e1.execute({id:6,gate:true,fn:mfn(6,10),cb:mcb(6),timeout:100})
-      e1.execute({id:7,fn:mfn(7),cb:mcb(7,10),timeout:100})
-    }, 200)
   })
-
-
-  it('determinism', function (fin) {
-    var e1 = executor({
-      trace:true,
-      timeout:100,
-      stubs:timerstub
-    })
-
-    var log = []
-    function mfn (id,t) {
-      return function(done) {
-        log.push(id)
-        if( t ) {
-          setTimeout(done,t)
-        }
-        else done()
-      }
-    }
-
-    var cI = 0
-    e1.on('clear', function () {
-      cI++
-      log.push('c'+cI)
-      //console.log(cI, e1.tracelog)
-
-      if( 4 === cI ) {
-        //console.log(log)
-
-        expect(log).to.deep.equal(
-          [ 0, 1, 'c1', 2, 3, 'c2', 4, 6, 5, 'c3', 9, 7, 10, 8, 'c4' ]
-        )
-
-        fin()
-      }
-    })
-
-    // seq: 0,1
-    e1.execute({id:0,fn:mfn(0)})
-    e1.execute({id:1,fn:mfn(1)})
-
-    setTimeout( function() {
-
-      // seq: 2,3
-      e1.execute({id:2,fn:mfn(2,40)})
-      e1.execute({id:3,fn:mfn(3,20)})
-
-      setTimeout( function() {
-
-        // seq: 4,6,5
-        e1.execute({id:4,gate:true,fn:mfn(4)})
-        e1.execute({id:5,fn:mfn(5)})
-        e1.execute({id:6,gate:true,fn:mfn(6)})
-
-        setTimeout( function() {
-
-          // seq: 9,7,10,8
-          e1.execute({id:7,gate:true,fn:mfn(7)})
-          e1.execute({id:8,fn:mfn(8)})
-          e1.execute({id:9,ungate:true,fn:mfn(9)})
-          e1.execute({id:10,gate:true,fn:mfn(10)})
-        },100)
-      },100)
-    },100)
-  })
-
-
-  it('errors', function (fin) {
-    var e1 = executor({
-      trace:true,
-      timeout:100,
-      stubs:timerstub
-    })
-
-
-    var log = []
-
-    e1.on('error',function(err) {
-      log.push('EE~'+err.message)
-    })
-
-
-    var MODE = {
-      ok:0,
-      throw:1,
-      cberr:2
-    }
-
-    function mfn (mode,id,t) {
-      return function(done) {
-        log.push(id)
-
-        if( MODE.throw === mode ) {
-          throw new Error('AAA')
-        }
-
-        var err = MODE.cberr === mode ? new Error('BBB') : null
-
-        if( t ) {
-          setTimeout(function(){done(err,id)},t)
-        }
-        else done(err,id)
-      }
-    }
-
-    function mcb (mode,id) {
-      return function(err,out) {
-        //console.log('c',id,err,out)
-        log.push('c'+id+'~'+(err&&err.message)+'~'+out)
-
-        if( MODE.throw === mode ) throw new Error('CCC')
-      }
-    }
-
-    var cI = 0
-    e1.on('clear', function () {
-      cI++
-      log.push('c'+cI)
-      //console.log(cI, e1.tracelog)
-      //console.log(log)
-
-      if( 1 === cI ) {
-        //console.log(log)
-
-        expect(log).to.deep.equal(
-          [ 0,
-            'c0~null~0',
-            1,
-            'c1~gate-executor: AAA~null',
-            2,
-            'c2~gate-executor: BBB~2',
-            3,
-            'c3~null~3',
-            'EE~gate-executor: CCC',
-            'c1' ]
-        )
-        fin()
-      }
-    })
-
-    // seq: 0,1
-    e1.execute({id:0,fn:mfn(MODE.ok,0),cb:mcb(MODE.ok,0)})
-    e1.execute({id:1,fn:mfn(MODE.throw,1),cb:mcb(MODE.ok,1)})
-    e1.execute({id:2,fn:mfn(MODE.cberr,2),cb:mcb(MODE.ok,2)})
-    e1.execute({id:3,fn:mfn(MODE.ok,3),cb:mcb(MODE.throw,3)})
-  })
-
+*/
 })
